fd6868cf4aec8be32ab136c500499f17329a2d64
adrian valencia hernandez <adrian15996@gmail.com>
2022-01-14 10:41:15 -0600

    plugin de autopausa agregado

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,12 @@
 import MediaPlayer from './mediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
+
 const video = document.querySelector("video");
 const PlayPause = document.querySelector(".PlayPause");
 const MuteUnmute = document.querySelector(".MuteUnmute");
 
-const player = new MediaPlayer({el:video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({el:video, plugins: [new AutoPlay(),new AutoPause(),]
+});
 PlayPause.onclick = ()=> player.togglePlay();
 MuteUnmute.onclick = ()=> player.toggleMute();
