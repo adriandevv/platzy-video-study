aec933fed2b9ede0df75beb589433cade5149fa1
Adrian Valencia Hernandez <adrian15996@gmail.com>
2023-12-02 15:14:41 -0600

    migration parcel v2

---
+++ dist/ejercicios/typescript/index.b558471a.js
@@ -0,0 +1,44 @@
+// //Boolean
+// let muted:boolean = true;
+// muted = false;
+// //Numbers
+// let age=6;
+// let enumerador:number = 42;
+// let denominador:number = age;
+// let result = enumerador/denominador;
+// //String
+// let nombre:string = 'adrian';
+// let saludo = `Me llamo ${nombre}`;
+// //arreglos
+// let people:string[]=[];
+// people = ['isabel','nicole','raul'];
+// // people.push(9000);
+// let peopleAndNumbers: Array<string | number> = [];
+// peopleAndNumbers.push("ricardo");
+// //Enum
+// enum Color{
+//     Rojo = "rojo",
+//     Verde = "verde",
+//     Azul = "azul",
+// }
+// let colorFavorito: Color = Color.Azul;
+// console.log(`Mi color Favorito es ${colorFavorito}`)
+//funciones
+// function add(a:number, b:number):number{
+//     return a + b;
+// }
+// const sum = add(5,9);
+// console.log(sum);
+// function crateAdder (a:number):(number) => number{
+//     return function(b:number){
+//         return b+a;
+//     }
+// } 
+// const addFour = crateAdder(4);
+// const fourPlus6 = addFour(6);
+// function fullName(firstName:string, lastName?: string):string{
+//     return `${firstName} ${lastName}`;
+// }
+// const richard = fullName('Richard');
+
+//# sourceMappingURL=index.b558471a.js.map
